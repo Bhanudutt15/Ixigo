d32162bf7bce761cb9a707327db08f2c66f54fbd
Bhanu Dutt <bhanuduttojha15@gmail.com>
2021-03-12 23:13:46 +0530

    ntial commit

--- src/test/java/Test_Suite/Test_Ixigo/URL_LaunchTest.java
+++ src/test/java/Test_Suite/Test_Ixigo/URL_LaunchTest.java
@@ -15,6 +15,7 @@ import org.testng.annotations.Test;
 public class URL_LaunchTest {
 	WebDriver driver;
 		
+	//Setting up the browser launch
 	@Test(priority=1)
 	public void  setup() {
 	
@@ -27,44 +28,62 @@ public class URL_LaunchTest {
 	driver.get("https://www.ixigo.com/");
 	}
 	
+	//Verifying Home page of the ixigo
 	@Test(priority =2)
 	public void verifyPageTest() {
 		String title = "ixigo - Flights, IRCTC Train Booking, Bus Booking, Air Tickets & Hotels";
 	String actualTitle=driver.getTitle();
 	Assert.assertEquals(title, actualTitle);
 	}	
+	
+	//Entering the journey details and number of passengers
 	@Test(priority = 3)
 	public void journeyDetailTest() throws InterruptedException {
 	Thread.sleep(5000);
 	driver.findElement(By.xpath("//span[text()='Round Trip']")).click();
+	//Entering the origin city value
 	driver.findElement(By.xpath("//div[text()='From']")).click();
 	WebElement from = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/div[6]/div/div/div[1]/div/div[1]/input"));
 	from.sendKeys("Delhi");
 	Thread.sleep(2000);
 	from.sendKeys(Keys.ENTER);
+	
+	//Entering the destination city value
 	driver.findElement(By.xpath("//div[text()='To']")).click();
 	WebElement to =driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/div[6]/div/div/div[3]/div/div[1]/input"));
 	to.sendKeys("Blr");
 	Thread.sleep(2000);
 	to.sendKeys(Keys.ENTER);
+	
+	//Selecting departure date
 	driver.findElement(By.xpath("//input[@ placeholder='Depart']")).click();
 	driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[1]/table/tbody/tr/td/div[contains(text(),'25')]")).click();
 	Thread.sleep(2000);
+	
+	//Selecting return date
 	driver.findElement(By.xpath("//input[@ placeholder='Return']")).click();
 	driver.findElement(By.cssSelector("body > div.rd-container.flight-ret-cal.extra-bottom.arrow-animation.rd-container-attachment > div.rd-date > div:nth-child(1) > table > tbody > tr:nth-child(5) > td:nth-child(2) > div.day.has-info")).click();
+	
+	//Adding number of travellers
 	driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/div[6]/div/div/div[5]/div/div[1]/input")).click();
 	driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div[6]/div/div/div[5]/div/div[2]/div[2]/div[1]/div[2]/span[2]")).click();
 	}
+	
+	//Validating result page
 	@Test(priority =4)
 	public void flightSearchTest() {
 
 	driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/div[6]/div/div/div[6]/button/div")).click();
 	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	String actualTitle = "New Delhi - Bengaluru, Economy Flights, roundtrip, 25 Mar - 29 Mar";
-	String expectedTitle = driver.getTitle();
+	WebElement el1 = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/div/div[1]/div[1]/div/div[1]"));
+	
+	//Validating the result page by verifying result page title
+	String expectedTitle = "New Delhi - Bengaluru, Economy Flights, roundtrip, 25 Mar - 29 Mar";
+	String actualTitle = driver.getTitle();
 	Assert.assertEquals(expectedTitle, actualTitle);
 	}
 	
+	//Validating filter options
 	@Test(priority =5)
 	public void validateDetailsTest() {
 	String text ="Stops";
@@ -85,8 +104,14 @@ public class URL_LaunchTest {
 	System.out.print(t3);
 	Assert.assertEquals(airline, t3);
 	
+	//Selecting only non-Stop flights
 	driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/div/div[1]/div[1]/div/div[2]/div[1]/span/div[1]/span[1]/span")).click();
+	}
 	
+	//Printing the airline details
+	@Test(priority = 6)
+	public void airlineDetailsTest() {
+		
 		driver.quit();
 	}
 }
